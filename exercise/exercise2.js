67afa5382c653991b5040441f2dbe318773e71c6
Hazal <88984994+xgravellx@users.noreply.github.com>
2022-05-16 22:14:17 +0300

    exercise1

---
+++ exercise/exercise2.js
@@ -0,0 +1,10 @@
+/*
+Bir fahrenheit cinsinden sıcaklığı değişkene atayınız.
+Bu değerin Celcius dönüşümünü tamamlayınız.
+Hesaplama formulu: celcius = 5/9 * (fahrenheit - 32)
+*/
+
+let fahrDegree = parseInt(prompt("Fahrenheit giriniz:"), 60);
+let celciusDegree = (5 / 9) * (fahrDegree -32);
+
+console.log(`Fahrenheit: ${fahrDegree} Celcius: ${celciusDegree.toFixed(2)}`);
---
+++ exercise/exercise3.js
@@ -0,0 +1,10 @@
+/* 
+Bir yıl değerinin artık yıl olup olmadığını hesaplayınız.
+Bir yılın artık olması için ya 400 ile modu 0 olmalı ya da 4 ile modu 0 olmalı ve 100 ile modu 0 olmamalıdır.
+4100 false değer vermelidir.
+
+*/
+
+let yearEntered = parseInt(prompt("Yıl giriniz:"));
+let leapYear    = (yearEntered % 400 == 0 || (yearEntered % 4 == 0 && yearEntered % 100 != 0));
+console.log(`Girilen Yıl: ${yearEntered} Artık Yıl mı? ${leapYear}`);
